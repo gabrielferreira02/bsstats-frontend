6b4c06566308353eeea44f1ea314e8448341f716
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-02-26 15:42:27 -0300

    Update - Added route for brawlers page - Added providerHttpClient in app.config.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { BrawlersComponent } from './pages/brawlers/brawlers.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "brawlers", component: BrawlersComponent}
+];
